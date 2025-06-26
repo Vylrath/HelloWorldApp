6b80e0a78f0ababb1f123e74d55dc3ee2fd1b937
Brian Chambers <brianmchambers@proton.me>
2025-06-25 20:49:24 -0500

    Update assembly information and regenerate binaries for HelloWorldApp

--- obj/Debug/net9.0/HelloWorldApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436f4b20b58680f5174e31e14df6c06dcb4ff786")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
